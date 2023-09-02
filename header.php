2f820a68a05a98049f50ffca080301b8aa547596
mehedi891 <mdmehedihasan891@gmail.com>
2023-09-03 01:08:10 +0600

    fav icon added

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<?php
   if ( ! function_exists( '_wp_render_title_tag' ) ) {
     function theme_slug_render_title() {
