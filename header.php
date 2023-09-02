3b5878ce06eea706019bee9b1b45393f1061ddc7
mehedi891 <mdmehedihasan891@gmail.com>
2023-09-03 01:16:24 +0600

    fav icon resolved

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri()?>/assets/images/favicon.png">
 	<?php
   if ( ! function_exists( '_wp_render_title_tag' ) ) {
     function theme_slug_render_title() {
